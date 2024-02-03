86c7613d6fe03827f5c92ab76c3fd6cd33c38629
Swap <swap15.2014@gmail.com>
2024-02-03 23:24:17 +0300

    new updated

---
+++ src/test/java/ru/netology/service/CashbackTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.sevice.CashbackHackService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CashbackTest {
+    CashbackHackService cashbackHackService = new CashbackHackService();
+
+    @Test
+    public void CashbackTest() {
+        assertEquals(0, cashbackHackService.remain(-200));
+    }
+    @Test
+    public void CashbackTest1() {
+        assertEquals(0, cashbackHackService.remain(0));
+    }
+
+    @Test
+    public void CashbackTest2() {
+        assertEquals(999, cashbackHackService.remain(1));
+    }
+
+    @Test
+    public void CashbackTest3() {
+        assertEquals(1, cashbackHackService.remain(999));
+    }
+
+    @Test
+    public void CashbackTest4() {
+        assertEquals(0, cashbackHackService.remain(1000));
+    }
+}
